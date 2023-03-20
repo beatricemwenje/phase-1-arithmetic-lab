d08c10e728cf3acf885a219199b05a73ed8a3026
beatrice <beatrice.mwenje@student.moringaschool>
2023-03-20 20:52:44 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// function multiply() {}
+let num1 = 31; 
+let num2 = 2; 
+let multiply = num1 * num2; 
+multiply = 62;
 
-// Write your code here
+// function random() {}
+let random = Math.floor(Math.random()*100);
+
+// function mod() {}
+let num3 = 36; 
+let num4 = 8; 
+let mod = num3 % num4;
+
+
+// function max () {}
+let max = Math.max(1, 4, 5, 20, 6, 7); 
+
+
+
+
+
